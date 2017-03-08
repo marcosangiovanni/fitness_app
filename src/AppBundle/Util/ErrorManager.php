8dc07f97851b586ba5ffc3c871b8ba970e061712
sanchota <sancho@eboox.it>
2017-03-08 11:21:54 +0100

    specificato error code per token non autorizzato

--- src/AppBundle/Util/ErrorManager.php
+++ src/AppBundle/Util/ErrorManager.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace AppBundle\Util;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class ErrorManager{
 	
 	public function createErrorArrayFromException(\Exception $e){
-		return array('code' => $e->getCode(),'message' => $e->getMessage());
+		$code = null;
+		if($e instanceof AuthenticationException){
+			$code = 1000;
+		}
+		return array('code' => $code ? $code : $e->getCode(),'message' => $e->getMessage());
 	}
 	
 }
